294479e2cd99e3e1aa7c6356bcf8a2b38fe416cf
Joshua Tellander <joshua.tellander@wisconsin.gov>
2017-08-09 09:42:41 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.listen(1701, function() {
+    console.log("Web server is running...");
+});
